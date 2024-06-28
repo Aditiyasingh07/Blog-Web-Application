8320f841e26d7a0d381656057e38954a293029e4
Aditiya singh <adisingh20012110@gmail.com>
2024-06-28 22:04:28 +0530

    blog page

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  // base: '/Blog-Web-Application/'
+  base: '/Blog-Web-Application/'
 });
-=======
-  });
->>>>>>> 576b63b16b6d76d2956bf467345c2e455620e8f7
